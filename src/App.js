d442426adff09d2ed7bc3ed83183f82e946feb34
Spyros Dimitroulas <ioannina.spyros1986@gmail.com>
2021-07-10 23:35:25 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Game from './components/game'
 import calculateWinner from './libraries/ticTacToe';
 
 function App(props) {
-    const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
-    const [stepNumber, setStepNumber] = useState(0);
-    const [xIsNext, setXisNext] = useState(true);
+  // const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  // const [stepNumber, setStepNumber] = useState(0);
+  // const [xIsNext, setXisNext] = useState(true);
+
+  const initialState = {
+    history: [{squares: Array(9).fill(null)}], 
+    stepNumber: 0, 
+    xIsNext: true
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function reducer(state, action) {
+    // ....
+  }
   
-    const play = (i) => {
-      const nHistory = [...history]; 
-      const current = nHistory[nHistory.length - 1];
-      const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = xIsNext ? 'X' : 'O';
-      setHistory(nHistory.concat([{ squares }]));
-      setStepNumber(nHistory.length);
-      setXisNext(!xIsNext);
+  const play = (i) => {
+    const nHistory = [...history]; 
+    const current = nHistory[nHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-    
-    const jumpTo = (step) => {
-      setStepNumber(step);
-      setXisNext((step % 2) === 0);
-    }
-
-    const current = history[stepNumber]; 
-    const winner = calculateWinner(current.squares);
-    const status = 
-      winner
-      ? 'Winner: ' + winner
-      : 'Next player: ' + (xIsNext ? 'X' : 'O');
-
-    return (
-       <Game 
-           play={play}
-           jumpTo={jumpTo}
-           status={status}
-           history={history}
-       />   
-    );
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory(nHistory.concat([{ squares }]));
+    setStepNumber(nHistory.length);
+    setXisNext(!xIsNext);
+  }
+  
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext((step % 2) === 0);
   }
 
-  export default App;
+  const current = history[stepNumber]; 
+  const winner = calculateWinner(current.squares);
+  const status = 
+    winner
+    ? 'Winner: ' + winner
+    : 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  return (
+      <Game 
+          play={play}
+          jumpTo={jumpTo}
+          status={status}
+          history={history}
+      />   
+  );
+}
+
+export default App;
